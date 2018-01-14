8290102e421a47038a61a3a87f141a78d0aaaaff
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-14 07:15:46 -0500

    Set player animation to run and added obstacle script

---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	private float speed = -3f;
+
+	private Rigidbody2D myBody;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		myBody.velocity = new Vector2 (speed, 0f);
+	}
+		
+} // Obstacle
